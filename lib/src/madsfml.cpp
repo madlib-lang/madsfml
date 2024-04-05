d42da79d1929bf21c2eea6a81f851c37621e8cb0
Arnaud BOEGLIN <a.boeglin67@gmail.com>
2024-04-05 22:23:08 +0200

    feat: extend events

--- lib/src/madsfml.cpp
+++ lib/src/madsfml.cpp
@@ -2,6 +2,7 @@
 
 #include "gc.h"
 #include "maybe.hpp"
+#include "number.hpp"
 #include "record.hpp"
 
 #ifdef __cplusplus
@@ -24,7 +25,6 @@ enum madsfml__EventType {
   MouseEnter,
   MouseLeave,
   MouseMove,
-  MouseWheelMove,
   MouseWheelScroll,
   SensorChange,
   Size,
@@ -33,6 +33,15 @@ enum madsfml__EventType {
   TouchMove,
   TouchStart,
 };
+
+enum madsfml__WheelType {
+  HorizontalWheel,
+  VerticalWheel,
+};
+
+typedef struct madsfml__Wheel {
+  int64_t index;
+} madsfml__Wheel_t;
 
 typedef struct madsfml__Event {
   int64_t index;
@@ -48,8 +57,8 @@ madsfml__Event_t *buildNullaryEvent(madsfml__EventType index) {
 }
 
 madlib__maybe__Maybe_t *madsfml__pollEvent(sf::RenderWindow *window) {
-  sf::Event event;
-  bool hasEvent = window->pollEvent(event);
+  sf::Event sfmlEvent;
+  bool hasEvent = window->pollEvent(sfmlEvent);
 
   madlib__maybe__Maybe_t *result =
       (madlib__maybe__Maybe_t *)GC_MALLOC(sizeof(madlib__maybe__Maybe_t));
@@ -60,7 +69,8 @@ madlib__maybe__Maybe_t *madsfml__pollEvent(sf::RenderWindow *window) {
     return result;
   }
 
-  switch (event.type) {
+  switch (sfmlEvent.type) {
+    case sf::Event::EventType::KeyReleased:
     case sf::Event::EventType::KeyPressed: {
       madlib__record__Record_t *eventData =
           (madlib__record__Record_t *)GC_MALLOC(
@@ -71,31 +81,31 @@ madlib__maybe__Maybe_t *madsfml__pollEvent(sf::RenderWindow *window) {
       eventData->fields[0] =
           (madlib__record__Field_t *)GC_MALLOC(sizeof(madlib__record__Field_t));
       eventData->fields[0]->name = (char *)"alt";
-      eventData->fields[0]->value = (void *)event.key.alt;
+      eventData->fields[0]->value = (void *)sfmlEvent.key.alt;
       eventData->fields[1] =
           (madlib__record__Field_t *)GC_MALLOC(sizeof(madlib__record__Field_t));
       eventData->fields[1]->name = (char *)"code";
-      eventData->fields[1]->value = (void *)event.key.code;
+      eventData->fields[1]->value = (void *)sfmlEvent.key.code;
       eventData->fields[2] =
           (madlib__record__Field_t *)GC_MALLOC(sizeof(madlib__record__Field_t));
       eventData->fields[2]->name = (char *)"control";
-      eventData->fields[2]->value = (void *)event.key.control;
+      eventData->fields[2]->value = (void *)sfmlEvent.key.control;
       eventData->fields[3] =
           (madlib__record__Field_t *)GC_MALLOC(sizeof(madlib__record__Field_t));
       eventData->fields[3]->name = (char *)"scanCode";
-      eventData->fields[3]->value = (void *)event.key.scancode;
+      eventData->fields[3]->value = (void *)sfmlEvent.key.scancode;
       eventData->fields[4] =
           (madlib__record__Field_t *)GC_MALLOC(sizeof(madlib__record__Field_t));
       eventData->fields[4]->name = (char *)"shift";
-      eventData->fields[4]->value = (void *)event.key.shift;
+      eventData->fields[4]->value = (void *)sfmlEvent.key.shift;
       eventData->fields[5] =
           (madlib__record__Field_t *)GC_MALLOC(sizeof(madlib__record__Field_t));
       eventData->fields[5]->name = (char *)"system";
-      eventData->fields[5]->value = (void *)event.key.system;
+      eventData->fields[5]->value = (void *)sfmlEvent.key.system;
 
       madsfml__Event_t *event =
           (madsfml__Event_t *)GC_MALLOC(sizeof(madsfml__Event_t));
-      event->index = madsfml__EventType::KeyPressed;
+      event->index = sfmlEvent.type == sf::Event::EventType::KeyPressed ? madsfml__EventType::KeyPressed : madsfml__EventType::KeyReleased;
       event->data = (void *)eventData;
 
       result->index = madlib__maybe__Maybe_JUST_INDEX;
@@ -113,15 +123,15 @@ madlib__maybe__Maybe_t *madsfml__pollEvent(sf::RenderWindow *window) {
       eventData->fields[0] =
           (madlib__record__Field_t *)GC_MALLOC(sizeof(madlib__record__Field_t));
       eventData->fields[0]->name = (char *)"button";
-      eventData->fields[0]->value = (void *)event.mouseButton.button;
+      eventData->fields[0]->value = (void *)sfmlEvent.mouseButton.button;
       eventData->fields[1] =
           (madlib__record__Field_t *)GC_MALLOC(sizeof(madlib__record__Field_t));
       eventData->fields[1]->name = (char *)"x";
-      eventData->fields[1]->value = (void *)event.mouseButton.x;
+      eventData->fields[1]->value = (void *)sfmlEvent.mouseButton.x;
       eventData->fields[2] =
           (madlib__record__Field_t *)GC_MALLOC(sizeof(madlib__record__Field_t));
       eventData->fields[2]->name = (char *)"y";
-      eventData->fields[2]->value = (void *)event.mouseButton.y;
+      eventData->fields[2]->value = (void *)sfmlEvent.mouseButton.y;
 
       madsfml__Event_t *event =
           (madsfml__Event_t *)GC_MALLOC(sizeof(madsfml__Event_t));
@@ -143,15 +153,15 @@ madlib__maybe__Maybe_t *madsfml__pollEvent(sf::RenderWindow *window) {
       eventData->fields[0] =
           (madlib__record__Field_t *)GC_MALLOC(sizeof(madlib__record__Field_t));
       eventData->fields[0]->name = (char *)"button";
-      eventData->fields[0]->value = (void *)event.mouseButton.button;
+      eventData->fields[0]->value = (void *)sfmlEvent.mouseButton.button;
       eventData->fields[1] =
           (madlib__record__Field_t *)GC_MALLOC(sizeof(madlib__record__Field_t));
       eventData->fields[1]->name = (char *)"x";
-      eventData->fields[1]->value = (void *)event.mouseButton.x;
+      eventData->fields[1]->value = (void *)sfmlEvent.mouseButton.x;
       eventData->fields[2] =
           (madlib__record__Field_t *)GC_MALLOC(sizeof(madlib__record__Field_t));
       eventData->fields[2]->name = (char *)"y";
-      eventData->fields[2]->value = (void *)event.mouseButton.y;
+      eventData->fields[2]->value = (void *)sfmlEvent.mouseButton.y;
 
       madsfml__Event_t *event =
           (madsfml__Event_t *)GC_MALLOC(sizeof(madsfml__Event_t));
@@ -173,11 +183,11 @@ madlib__maybe__Maybe_t *madsfml__pollEvent(sf::RenderWindow *window) {
       eventData->fields[0] =
           (madlib__record__Field_t *)GC_MALLOC(sizeof(madlib__record__Field_t));
       eventData->fields[0]->name = (char *)"x";
-      eventData->fields[0]->value = (void *)event.mouseMove.x;
+      eventData->fields[0]->value = (void *)sfmlEvent.mouseMove.x;
       eventData->fields[1] =
           (madlib__record__Field_t *)GC_MALLOC(sizeof(madlib__record__Field_t));
       eventData->fields[1]->name = (char *)"y";
-      eventData->fields[1]->value = (void *)event.mouseMove.y;
+      eventData->fields[1]->value = (void *)sfmlEvent.mouseMove.y;
 
       madsfml__Event_t *event =
           (madsfml__Event_t *)GC_MALLOC(sizeof(madsfml__Event_t));
@@ -219,7 +229,75 @@ madlib__maybe__Maybe_t *madsfml__pollEvent(sf::RenderWindow *window) {
       break;
     }
 
+    case sf::Event::EventType::Resized: {
+      madlib__record__Record_t *eventData =
+          (madlib__record__Record_t *)GC_MALLOC(
+              sizeof(madlib__record__Record_t));
+      eventData->fieldCount = 2;
+      eventData->fields = (madlib__record__Field_t **)GC_MALLOC(
+          sizeof(madlib__record__Field_t *) * 2);
+      eventData->fields[0] =
+          (madlib__record__Field_t *)GC_MALLOC(sizeof(madlib__record__Field_t));
+      eventData->fields[0]->name = (char *)"width";
+      eventData->fields[0]->value = (void *)sfmlEvent.mouseMove.x;
+      eventData->fields[1] =
+          (madlib__record__Field_t *)GC_MALLOC(sizeof(madlib__record__Field_t));
+      eventData->fields[1]->name = (char *)"height";
+      eventData->fields[1]->value = (void *)sfmlEvent.mouseMove.y;
+
+      madsfml__Event_t *event =
+          (madsfml__Event_t *)GC_MALLOC(sizeof(madsfml__Event_t));
+      event->index = madsfml__EventType::Size;
+      event->data = (void *)eventData;
+
+      result->index = madlib__maybe__Maybe_JUST_INDEX;
+      result->data = event;
+      break;
+    }
+
+    case sf::Event::EventType::TextEntered: {
+      result->index = madlib__maybe__Maybe_JUST_INDEX;
+      result->data = (void*) sfmlEvent.text.unicode;
+    }
+
+    case sf::Event::EventType::MouseWheelScrolled: {
+      madlib__record__Record_t *eventData =
+          (madlib__record__Record_t *)GC_MALLOC(
+              sizeof(madlib__record__Record_t));
+      eventData->fieldCount = 4;
+      eventData->fields = (madlib__record__Field_t **)GC_MALLOC(
+          sizeof(madlib__record__Field_t *) * 4);
+      eventData->fields[0] =
+          (madlib__record__Field_t *)GC_MALLOC(sizeof(madlib__record__Field_t));
+      eventData->fields[0]->name = (char *)"delta";
+      eventData->fields[0]->value = (void *) boxDouble(sfmlEvent.mouseWheelScroll.delta);
+      eventData->fields[1] =
+          (madlib__record__Field_t *)GC_MALLOC(sizeof(madlib__record__Field_t));
+      eventData->fields[1]->name = (char *)"wheel";
+      madsfml__Wheel_t* wheel = (madsfml__Wheel_t*) GC_MALLOC(sizeof(madsfml__Wheel_t));
+      wheel->index = sfmlEvent.mouseWheelScroll.wheel == sf::Mouse::Wheel::HorizontalWheel ? HorizontalWheel : VerticalWheel;
+      eventData->fields[1]->value = (void *) wheel;
+      eventData->fields[2] =
+          (madlib__record__Field_t *)GC_MALLOC(sizeof(madlib__record__Field_t));
+      eventData->fields[2]->name = (char *)"x";
+      eventData->fields[2]->value = (void *)sfmlEvent.mouseWheelScroll.x;
+      eventData->fields[3] =
+          (madlib__record__Field_t *)GC_MALLOC(sizeof(madlib__record__Field_t));
+      eventData->fields[3]->name = (char *)"y";
+      eventData->fields[3]->value = (void *)sfmlEvent.mouseWheelScroll.y;
+
+      madsfml__Event_t *event =
+          (madsfml__Event_t *)GC_MALLOC(sizeof(madsfml__Event_t));
+      event->index = madsfml__EventType::MouseWheelScroll;
+      event->data = (void *)eventData;
+
+      result->index = madlib__maybe__Maybe_JUST_INDEX;
+      result->data = (void*) event;
+      break;
+    }
+
     default: {
+      std::cout << sfmlEvent.type << std::endl;
       result->index = madlib__maybe__Maybe_NOTHING_INDEX;
       break;
     }
--- lib/src/window.cpp
+++ lib/src/window.cpp
@@ -8,15 +8,19 @@ extern "C" {
 #endif
 
 sf::RenderWindow *madsfml__window__create(int32_t w, int32_t h, char *name) {
-  return new sf::RenderWindow(sf::VideoMode(w, h), std::string(name));
+  sf::ContextSettings contextSettings;
+  contextSettings.antialiasingLevel = 8;
+  contextSettings.depthBits = 24;
+  contextSettings.sRgbCapable = false;
+
+  // Create the main window
+  return new sf::RenderWindow(sf::VideoMode(w, h), name, sf::Style::Default, contextSettings);
 }
 
-sf::RenderWindow *madsfml__window__createWithSettings(
-    int32_t w, int32_t h, char *name, int32_t antialiasingLevel) {
+sf::RenderWindow *madsfml__window__createWithSettings(int32_t w, int32_t h, char *name, int32_t antialiasingLevel) {
   sf::ContextSettings settings;
   settings.antialiasingLevel = antialiasingLevel;
-  return new sf::RenderWindow(sf::VideoMode(w, h), std::string(name),
-                              sf::Style::Default, settings);
+  return new sf::RenderWindow(sf::VideoMode(w, h), std::string(name), sf::Style::Default, settings);
 }
 
 void madsfml__window__close(sf::RenderWindow *window) { window->close(); }
@@ -28,29 +32,24 @@ sf::RenderWindow *madsfml__window__setFrameRateLimit(int32_t limit, sf::RenderWi
   return window;
 }
 
-sf::View *madsfml__window__getView(sf::RenderWindow *window) {
-  return (sf::View *)&window->getView();
-}
+sf::View *madsfml__window__getView(sf::RenderWindow *window) { return (sf::View *)&window->getView(); }
 
-sf::RenderWindow *madsfml__window__setView(sf::View *view,
-                                           sf::RenderWindow *window) {
+sf::RenderWindow *madsfml__window__setView(sf::View *view, sf::RenderWindow *window) {
   window->setView(*view);
   return window;
 }
 
-void madsfml__window__clear(int32_t color, sf::RenderWindow *window) {
-  window->clear(sf::Color(color));
-}
+void madsfml__window__clear(int32_t color, sf::RenderWindow *window) { window->clear(sf::Color(color)); }
 
-sf::View *madsfml__window__getDefaultView(sf::RenderWindow *window) {
-  return (sf::View *)&window->getDefaultView();
-}
+sf::View *madsfml__window__getDefaultView(sf::RenderWindow *window) { return (sf::View *)&window->getDefaultView(); }
 
-madsfml__math__vector2_t *madsfml__window__mapPixelToCoords(int32_t x, int32_t y, sf::View *view, sf::RenderWindow *window) {
+madsfml__math__vector2_t *madsfml__window__mapPixelToCoords(int32_t x, int32_t y, sf::View *view,
+                                                            sf::RenderWindow *window) {
   return madsfml__math__toMadlibFloatVector2(window->mapPixelToCoords(sf::Vector2i(x, y), *view));
 }
 
-madsfml__math__vector2_t *madsfml__window__mapCoordsToPixel(double x, double y, sf::View *view, sf::RenderWindow *window) {
+madsfml__math__vector2_t *madsfml__window__mapCoordsToPixel(double x, double y, sf::View *view,
+                                                            sf::RenderWindow *window) {
   return madsfml__math__toMadlibIntVector2(window->mapCoordsToPixel(sf::Vector2f(x, y), *view));
 }
 
